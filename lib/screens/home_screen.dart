6ee059f72f48d7540e28d33ee8c6893ea35803f2
JoseLuisGit <joseluisstacruz@gmail.com>
2022-03-19 11:37:57 -0400

    design home navigation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
 
+import 'package:qr_app/widgets/custom_bottom_navigation_bar.dart';
+import 'package:qr_app/widgets/scan_button.dart';
+
 class HomeScreen extends StatelessWidget {
    
   const HomeScreen({Key? key}) : super(key: key);
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        title: Text('Historial'),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.delete))
+        ],
+      ),
       body: Center(
          child: Text('HomeScreen'),
       ),
+      bottomNavigationBar: CustomBottomNavigationBar(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
---
+++ lib/widgets/custom_bottom_navigation_bar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  const CustomBottomNavigationBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.map), label: 'Map'),
+      ],
+    );
+  }
+}
